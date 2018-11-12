c242d84d719dc3edbf37c8940c119e6bfef3108a
Shawn Treichel <shawn.treichel1@gmail.com>
2018-11-12 13:08:13 -0500

    Update Program.cs

--- DellBiosInstaller/DellBiosInstaller/Program.cs
+++ DellBiosInstaller/DellBiosInstaller/Program.cs
@@ -9,29 +9,29 @@ namespace BiosDownloader
     {
         static void Main(string[] args)
         {
-            string biosDownloadLink = Methods.GetDownloadLink(); //Get BIOS download link for current machine            
+            string biosDownloadLink = Utils.GetDownloadLink(); //Get BIOS download link for current machine            
             string fileName = biosDownloadLink.Split('/').Last(); //Get filename of BIOS to name downloaded file
             string downloadDir = Path.Combine(Path.GetTempPath() + @"BIOS\");
             string theLatestBios = fileName.Split('_').Last().Substring(0, fileName.Split('_').Last().Length - 4); //Get the BIOS verion number from Dell's website
-            string currentBIOS = Methods.GetCurrentBIOS(); //Get current machine's BIOS version
-            Methods.LogActionToFile("Retrieved machine specific BIOS link");
+            string currentBIOS = Utils.GetCurrentBIOS(); //Get current machine's BIOS version
+            Utils.LogActionToFile("Retrieved machine specific BIOS link");
 
-            Methods.LogActionToFile($"The latest BIOS is {theLatestBios}, the current version is {currentBIOS}");
+            Utils.LogActionToFile($"The latest BIOS is {theLatestBios}, the current version is {currentBIOS}");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"The latest BIOS is {theLatestBios}, the current version is {currentBIOS}");
 
             if (currentBIOS == theLatestBios) //If the current version is the latest then there is no need to update
             {
                 Console.ForegroundColor = ConsoleColor.Magenta;
-                Methods.LogActionToFile("BIOS Version is already up to date.");
+                Utils.LogActionToFile("BIOS Version is already up to date.");
                 Console.WriteLine("BIOS Version is already up to date. Closing.");
                 Thread.Sleep(2000);
                 Environment.Exit(0);
             }
             else //Current version is not latest version, update needed
             {
-                Methods.DownloadFile(biosDownloadLink, fileName); //Download the file and name it accordingly
-                Methods.StartUpgrade(downloadDir, fileName); //Start process executable
+                Utils.DownloadFile(biosDownloadLink, fileName); //Download the file and name it accordingly
+                Utils.StartUpgrade(downloadDir, fileName); //Start process executable
             }
         }     
     }
